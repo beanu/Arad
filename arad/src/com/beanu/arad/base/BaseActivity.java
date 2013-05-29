1fa856345a593695e49b6fb9d0554b68f1df851c
beanu <zhaoyunhello@gmail.com>
2013-05-29 16:48:08 +0800

    add activity sliding gesturedetector

--- arad/src/com/beanu/arad/base/BaseActivity.java
+++ arad/src/com/beanu/arad/base/BaseActivity.java
@@ -1,17 +1,33 @@
 package com.beanu.arad.base;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 
-public class BaseActivity extends SherlockFragmentActivity {
+public class BaseActivity extends SherlockFragmentActivity implements OnGestureListener {
+
+	private GestureDetector mDetector;
+	private boolean mEnableGD;
+	private SlidingEventListener slidingListener;
+
+	/**
+	 * 设置左右滑动的事件,前提是enableSlideGestureDetector(true)
+	 */
+	public interface SlidingEventListener {
+		void leftSlidingEvent();
+
+		void rightSlidingEvent();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-//		setTheme(R.style.Theme_Sherlock_Light);
 		super.onCreate(savedInstanceState);
-
+		mEnableGD = false;
+		mDetector = new GestureDetector(this, this);
 	}
 
 	@Override
@@ -19,10 +35,80 @@ public class BaseActivity extends SherlockFragmentActivity {
 		switch (item.getItemId()) {
 		case android.R.id.home:// ActionBar左方的返回按钮，需要覆写
 			this.finish();
+			if (mEnableGD && slidingListener != null)
+				slidingListener.rightSlidingEvent();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
+	}
+	
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		return super.dispatchTouchEvent(ev);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (mEnableGD)
+			return mDetector.onTouchEvent(event);
+		return super.onTouchEvent(event);
+	}
+
+	/**
+	 * 是否设置左右滑动监听
+	 * 
+	 * @param enable
+	 */
+	public void enableSlideGestureDetector(boolean enable) {
+		mEnableGD = enable;
+	}
+
+	public void setSlidingEventListener(SlidingEventListener listener) {
+		this.slidingListener = listener;
+	}
+
+	/**
+	 * ***********************************************************************
+	 * *************************OnGestureListener*****************************
+	 */
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+		return false;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+		if (e1.getX() - e2.getX() > 50 && Math.abs(velocityX) > 0) {
+			if (slidingListener != null && mEnableGD) {
+				slidingListener.leftSlidingEvent();
+			}
+		} else if (e2.getX() - e1.getX() > 50 && Math.abs(velocityX) > 0) {
+			if (slidingListener != null && mEnableGD) {
+				slidingListener.rightSlidingEvent();
+			}
+		}
+
+		return false;
 	}
 
 }
