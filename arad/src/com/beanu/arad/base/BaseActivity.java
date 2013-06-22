6fda7a9c542faa140619130a372364f498efb9f3
beanu <zhaoyunhello@gmail.com>
2013-06-22 15:47:02 +0800

    add exception handle

--- arad/src/com/beanu/arad/base/BaseActivity.java
+++ arad/src/com/beanu/arad/base/BaseActivity.java
@@ -14,135 +14,133 @@ import android.widget.AbsListView;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 
+/**
+ * 1.添加IOC 注解功能 2.检测Actionbar的home 事件
+ * 
+ * @author beanu
+ * 
+ */
 public class BaseActivity extends SherlockFragmentActivity {
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:// ActionBar左方的返回按钮，需要覆写
+			this.finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:// ActionBar左方的返回按钮，需要覆写
-                this.finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-
-    protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	}
-	
 
 	public void setContentView(int layoutResID) {
 		super.setContentView(layoutResID);
 		initView();
 	}
 
-
 	public void setContentView(View view, LayoutParams params) {
 		super.setContentView(view, params);
 		initView();
 	}
-
-
 
 	public void setContentView(View view) {
 		super.setContentView(view);
 		initView();
 	}
 
-
-
-	private void initView(){
+	private void initView() {
 		Field[] fields = getClass().getDeclaredFields();
-		if(fields!=null && fields.length>0){
-			for(Field field : fields){
+		if (fields != null && fields.length > 0) {
+			for (Field field : fields) {
 				ViewInject viewInject = field.getAnnotation(ViewInject.class);
-				if(viewInject!=null){
+				if (viewInject != null) {
 					int viewId = viewInject.id();
 					try {
 						field.setAccessible(true);
-						field.set(this,findViewById(viewId));
+						field.set(this, findViewById(viewId));
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
-					
+
 					String clickMethod = viewInject.click();
-					if(!TextUtils.isEmpty(clickMethod))
-						setViewClickListener(field,clickMethod);
-					
+					if (!TextUtils.isEmpty(clickMethod))
+						setViewClickListener(field, clickMethod);
+
 					String longClickMethod = viewInject.longClick();
-					if(!TextUtils.isEmpty(longClickMethod))
-						setViewLongClickListener(field,longClickMethod);
-					
+					if (!TextUtils.isEmpty(longClickMethod))
+						setViewLongClickListener(field, longClickMethod);
+
 					String itemClickMethod = viewInject.itemClick();
-					if(!TextUtils.isEmpty(itemClickMethod))
-						setItemClickListener(field,itemClickMethod);
-					
+					if (!TextUtils.isEmpty(itemClickMethod))
+						setItemClickListener(field, itemClickMethod);
+
 					String itemLongClickMethod = viewInject.itemLongClick();
-					if(!TextUtils.isEmpty(itemLongClickMethod))
-						setItemLongClickListener(field,itemLongClickMethod);
-					
+					if (!TextUtils.isEmpty(itemLongClickMethod))
+						setItemLongClickListener(field, itemLongClickMethod);
+
 					Select select = viewInject.select();
-					if(!TextUtils.isEmpty(select.selected()))
-						setViewSelectListener(field,select.selected(),select.noSelected());
-					
+					if (!TextUtils.isEmpty(select.selected()))
+						setViewSelectListener(field, select.selected(), select.noSelected());
+
 				}
 			}
 		}
 	}
-	
-	
-	private void setViewClickListener(Field field,String clickMethod){
+
+	private void setViewClickListener(Field field, String clickMethod) {
 		try {
 			Object obj = field.get(this);
-			if(obj instanceof View){
-				((View)obj).setOnClickListener(new EventListener(this).click(clickMethod));
+			if (obj instanceof View) {
+				((View) obj).setOnClickListener(new EventListener(this).click(clickMethod));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private void setViewLongClickListener(Field field,String clickMethod){
+
+	private void setViewLongClickListener(Field field, String clickMethod) {
 		try {
 			Object obj = field.get(this);
-			if(obj instanceof View){
-				((View)obj).setOnLongClickListener(new EventListener(this).longClick(clickMethod));
+			if (obj instanceof View) {
+				((View) obj).setOnLongClickListener(new EventListener(this).longClick(clickMethod));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private void setItemClickListener(Field field,String itemClickMethod){
+
+	private void setItemClickListener(Field field, String itemClickMethod) {
 		try {
 			Object obj = field.get(this);
-			if(obj instanceof AbsListView){
-				((AbsListView)obj).setOnItemClickListener(new EventListener(this).itemClick(itemClickMethod));
+			if (obj instanceof AbsListView) {
+				((AbsListView) obj).setOnItemClickListener(new EventListener(this).itemClick(itemClickMethod));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private void setItemLongClickListener(Field field,String itemClickMethod){
+
+	private void setItemLongClickListener(Field field, String itemClickMethod) {
 		try {
 			Object obj = field.get(this);
-			if(obj instanceof AbsListView){
-				((AbsListView)obj).setOnItemLongClickListener(new EventListener(this).itemLongClick(itemClickMethod));
+			if (obj instanceof AbsListView) {
+				((AbsListView) obj).setOnItemLongClickListener(new EventListener(this).itemLongClick(itemClickMethod));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private void setViewSelectListener(Field field,String select,String noSelect){
+
+	private void setViewSelectListener(Field field, String select, String noSelect) {
 		try {
 			Object obj = field.get(this);
-			if(obj instanceof View){
-				((AbsListView)obj).setOnItemSelectedListener(new EventListener(this).select(select).noSelect(noSelect));
+			if (obj instanceof View) {
+				((AbsListView) obj)
+						.setOnItemSelectedListener(new EventListener(this).select(select).noSelect(noSelect));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
