8e588d1bb14e03e4aad25f94cac9419a9afe5774
beanu <zhaoyunhello@gmail.com>
2013-09-10 14:54:20 +0800

    修复http jar包

---
+++ arad/src/com/beanu/arad/http/INetResult.java
@@ -0,0 +1,19 @@
+package com.beanu.arad.http;
+
+/**
+ * Activity 访问网络接口 MVC模式
+ * 
+ * @author beanu
+ * 
+ */
+public interface INetResult {
+
+	/**
+	 * 
+	 * @param success
+	 *            网络访问成功还是失败
+	 */
+	public void onSuccess();
+
+	public void onFaild(String errorMessage);
+}
