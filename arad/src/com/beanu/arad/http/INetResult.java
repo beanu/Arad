1b191d0613b39549652b9ed9453794f2ff9a8eca
beanu <zhaoyunhello@gmail.com>
2013-09-18 16:59:55 +0800

    修改网络返回借口

--- arad/src/com/beanu/arad/http/INetResult.java
+++ arad/src/com/beanu/arad/http/INetResult.java
@@ -13,7 +13,7 @@ public interface INetResult {
 	 * @param success
 	 *            网络访问成功还是失败
 	 */
-	public void onSuccess();
+	public void onSuccess(int type);
 
 	public void onFaild(String errorMessage);
 }
