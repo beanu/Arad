776a3f031b2f29fcf7302731f86776f97a18bf31
Beanu <zhaoyunhello@gmail.com>
2014-08-03 20:26:16 +0800

    minsdk 8

--- arad/src/com/beanu/arad/Http.java
+++ arad/src/com/beanu/arad/Http.java
@@ -6,6 +6,14 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.BinaryHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+
 public class Http implements IHTTP {
 
     private static Http instance;
@@ -17,6 +25,12 @@ public class Http implements IHTTP {
     public static Http instance() {
         if (instance == null)
             instance = new Http();
+        ((DefaultHttpClient)client.getHttpClient()).addResponseInterceptor(new HttpResponseInterceptor() {
+            @Override
+            public void process(HttpResponse httpResponse, HttpContext httpContext) throws HttpException, IOException {
+
+            }
+        });
         return instance;
     }
 
