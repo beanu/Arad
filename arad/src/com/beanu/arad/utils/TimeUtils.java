1641ea06ec4ac198f381980d0eae6e08e81f951e
Beanu <zhaoyunhello@gmail.com>
2014-09-13 00:24:23 +0800

    update to v0.05

--- arad/src/com/beanu/arad/utils/TimeUtils.java
+++ arad/src/com/beanu/arad/utils/TimeUtils.java
@@ -20,39 +20,39 @@ import android.content.Context;
 
 public class TimeUtils {
 
-	private static final int SECOND = 1000;
-	private static final int MINUTE = 60 * SECOND;
-	private static final int HOUR = 60 * MINUTE;
-	private static final int DAY = 24 * HOUR;
+    private static final int SECOND = 1000;
+    private static final int MINUTE = 60 * SECOND;
+    private static final int HOUR = 60 * MINUTE;
+    private static final int DAY = 24 * HOUR;
 
-	public static String getTimeAgo(long time, Context ctx) {
-		// TODO: use DateUtils methods instead
-		if (time < 1000000000000L) {
-			// if timestamp given in seconds, convert to millis
-			time *= 1000;
-		}
+    public static String getTimeAgo(long time, Context ctx) {
+        // TODO: use DateUtils methods instead
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
 
-		long now = AndroidUtil.getCurrentTime(ctx);
-		if (time > now || time <= 0) {
-			return null;
-		}
+        long now = AndroidUtil.getCurrentTime(ctx);
+        if (time > now || time <= 0) {
+            return null;
+        }
 
-		final long diff = now - time;
-		if (diff < MINUTE) {
-			return "just now";
-		} else if (diff < 2 * MINUTE) {
-			return "a minute ago";
-		} else if (diff < 50 * MINUTE) {
-			return diff / MINUTE + " minutes ago";
-		} else if (diff < 90 * MINUTE) {
-			return "an hour ago";
-		} else if (diff < 24 * HOUR) {
-			return diff / HOUR + " hours ago";
-		} else if (diff < 48 * HOUR) {
-			return "yesterday";
-		} else {
-			return diff / DAY + " days ago";
-		}
-	}
+        final long diff = now - time;
+        if (diff < MINUTE) {
+            return "刚刚";
+        } else if (diff < 2 * MINUTE) {
+            return "1分钟前";
+        } else if (diff < 50 * MINUTE) {
+            return diff / MINUTE + " 分钟前";
+        } else if (diff < 90 * MINUTE) {
+            return "1小时前";
+        } else if (diff < 24 * HOUR) {
+            return diff / HOUR + " 小时前";
+        } else if (diff < 48 * HOUR) {
+            return "昨天";
+        } else {
+            return diff / DAY + " 天之前";
+        }
+    }
 
 }
