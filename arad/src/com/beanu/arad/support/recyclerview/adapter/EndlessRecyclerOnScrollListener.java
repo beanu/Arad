1995eae43d7c8018e01d2af02e5cf809c3b5764d
Beanu <zhaoyunhello@gmail.com>
2015-06-18 18:05:48 +0800

    修改loadmore方案

--- arad/src/com/beanu/arad/support/recyclerview/adapter/EndlessRecyclerOnScrollListener.java
+++ arad/src/com/beanu/arad/support/recyclerview/adapter/EndlessRecyclerOnScrollListener.java
@@ -1,7 +1,11 @@
 package com.beanu.arad.support.recyclerview.adapter;
 
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.beanu.arad.support.loadmore.ILoadMoreAdapter;
 
 /**
  * load more 监听
@@ -11,45 +15,33 @@ public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScr
 
     public static String TAG = EndlessRecyclerOnScrollListener.class.getSimpleName();
 
-    private int previousTotal = 0; // The total number of items in the dataset after the last load
-    private boolean loading = true; // True if we are still waiting for the last set of data to load.
-    private int visibleThreshold = 5; // The minimum amount of items to have below your current scroll position before loading more.
-    int firstVisibleItem, visibleItemCount, totalItemCount;
-
-    private int current_page = 1;
+    int lastVisiblePostion, totalItemCount;
 
     private LinearLayoutManager mLinearLayoutManager;
+    private ILoadMoreAdapter listener;
 
-    public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager) {
+
+    public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager, ILoadMoreAdapter listener) {
         this.mLinearLayoutManager = linearLayoutManager;
+        this.listener = listener;
     }
 
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
-
-        visibleItemCount = recyclerView.getChildCount();
         totalItemCount = mLinearLayoutManager.getItemCount();
-        firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+        lastVisiblePostion = mLinearLayoutManager.findLastVisibleItemPosition();
+    }
 
-        if (loading) {
-            if (totalItemCount > previousTotal) {
-                loading = false;
-                previousTotal = totalItemCount;
-            }
-        }
-        if (!loading && (totalItemCount - visibleItemCount)
-                <= (firstVisibleItem + visibleThreshold)) {
-            // End has been reached
-
-            // Do something
-            current_page++;
-
-            onLoadMore(current_page);
-
-            loading = true;
+    @Override
+    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+        super.onScrollStateChanged(recyclerView, newState);
+        if (newState == RecyclerView.SCROLL_STATE_IDLE && (lastVisiblePostion + 1) == totalItemCount) {
+            if (listener.hasMoreResults() && !listener.isLoading())
+                onLoadMore();
         }
     }
 
-    public abstract void onLoadMore(int current_page);
+    //TODO 正在加载的过程中，如果再上拉，还是能够执行此方法
+    public abstract void onLoadMore();
 }
