5cd27e6da97c828b204e9c8cc0627cc7e38e1171
zhao yunhe <Beanu@zhaodeMBP.lan>
2016-03-17 08:58:43 +0800

    更换pullscrollview，解决控件的事件监听问题

--- arad/src/com/beanu/arad/widget/PullScrollView.java
+++ arad/src/com/beanu/arad/widget/PullScrollView.java
@@ -2,10 +2,9 @@ package com.beanu.arad.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.PointF;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
@@ -14,113 +13,50 @@ import android.widget.ScrollView;
 import com.beanu.arad.R;
 
 /**
- * 自定义ScrollView,下拉背景伸缩回弹效果
- *
- * https://github.com/MarkMjw/PullScrollView/
- *
- * @author markmjw
- * @date 2013-09-13
+ * Created by harvic on 2015/6/17
+ * @adress blog.csdn.net/harvic880925
  */
 public class PullScrollView extends ScrollView {
+    //底部图片View
+    private View mHeaderView;
+    //头部图片的初始化位置
+    private Rect mHeadInitRect = new Rect();
+    //底部View
+    private View mContentView;
+    //ScrollView的contentView的初始化位置
+    private Rect mContentInitRect = new Rect();
 
-    private static final String LOG_TAG = "PullScrollView";
+    //初始点击位置
+    private Point mTouchPoint = new Point();
+
+
+    //标识当前view是否移动
+    boolean mIsMoving = false;
+
+    //是否禁止控件本身的的移动
+    boolean mEnableMoving = false;
+
+    //是否使用layout函数移动布局
+    boolean mIsLayout = false;
     /**
      * 阻尼系数,越小阻力就越大.
      */
     private static final float SCROLL_RATIO = 0.5f;
 
-    /**
-     * 滑动至翻转的距离.
-     */
-    private static final int TURN_DISTANCE = 100;
+    private int mContentTop, mContentBottom;
 
-    /**
-     * 头部view.
-     */
-    private View mHeader;
+    private int mHeaderCurTop, mHeaderCurBottom;
 
-    /**
-     * 头部view高度.
-     */
-    private int mHeaderHeight;
-
-    /**
-     * 头部view显示高度.
-     */
-    private int mHeaderVisibleHeight;
-
-    /**
-     * ScrollView的content view.
-     */
-    private View mContentView;
-
-    /**
-     * ScrollView的content view矩形.
-     */
-    private Rect mContentRect = new Rect();
-
-    /**
-     * 首次点击的Y坐标.
-     */
-    private PointF mStartPoint = new PointF();
-
-    /**
-     * 是否开始移动.
-     */
-    private boolean isMoving = false;
-
-    /**
-     * 是否移动到顶部位置.
-     */
-    private boolean isTop = false;
-
-    /**
-     * 头部图片初始顶部和底部.
-     */
-    private int mInitTop, mInitBottom;
-
-    /**
-     * 头部图片拖动时顶部和底部.
-     */
-    private int mCurrentTop, mCurrentBottom;
-
-    /**
-     * 状态变化时的监听器.
-     */
-    private OnTurnListener mOnTurnListener;
-
-    private enum State {
-        /**
-         * 顶部
-         */
-        UP,
-        /**
-         * 底部
-         */
-        DOWN,
-        /**
-         * 正常
-         */
-        NORMAL
-    }
-
-    /**
-     * 状态.
-     */
-    private State mState = State.NORMAL;
-
-    public PullScrollView(Context context) {
-        super(context);
-        init(context, null);
-    }
+    //用户定义的headview高度
+    private int mHeaderHeight = 0;
 
     public PullScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
     }
 
-    public PullScrollView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    public PullScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
         init(context, attrs);
     }
 
@@ -133,29 +69,14 @@ public class PullScrollView extends ScrollView {
 
             if (ta != null) {
                 mHeaderHeight = (int) ta.getDimension(R.styleable.PullScrollView_headerHeight, -1);
-                mHeaderVisibleHeight = (int) ta.getDimension(R.styleable
-                        .PullScrollView_headerVisibleHeight, -1);
                 ta.recycle();
+
             }
         }
     }
 
-    /**
-     * 设置Header
-     *
-     * @param view
-     */
-    public void setHeader(View view) {
-        mHeader = view;
-    }
-
-    /**
-     * 设置状态改变时的监听器
-     *
-     * @param turnListener
-     */
-    public void setOnTurnListener(OnTurnListener turnListener) {
-        mOnTurnListener = turnListener;
+    public void setmHeaderView(View view) {
+        mHeaderView = view;
     }
 
     @Override
@@ -163,177 +84,84 @@ public class PullScrollView extends ScrollView {
         if (getChildCount() > 0) {
             mContentView = getChildAt(0);
         }
+        super.onFinishInflate();
     }
 
     @Override
-    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-        super.onScrollChanged(l, t, oldl, oldt);
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE: {
+                int deltaY = (int) event.getY() - mTouchPoint.y;
+//                deltaY = deltaY < 0 ? 0 : (deltaY > mHeaderView.getHeight() ? mHeaderView.getHeight() : deltaY);
+                deltaY = deltaY < 0 ? 0 : (deltaY > mHeaderHeight ? mHeaderHeight : deltaY);
+                if (deltaY > 0 && deltaY >= getScrollY() && mIsLayout) {
+                    float headerMoveHeight = deltaY * 0.5f * SCROLL_RATIO;
+                    mHeaderCurTop = (int) (mHeadInitRect.top + headerMoveHeight);
+                    mHeaderCurBottom = (int) (mHeadInitRect.bottom + headerMoveHeight);
 
-        if (getScrollY() == 0) {
-            isTop = true;
-        }
-    }
+                    float contentMoveHeight = deltaY * SCROLL_RATIO;
+                    mContentTop = (int) (mContentInitRect.top + contentMoveHeight);
+                    mContentBottom = (int) (mContentInitRect.bottom + contentMoveHeight);
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return onTouchEvent(ev) || super.onInterceptTouchEvent(ev);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        if (mContentView != null) {
-            int action = ev.getAction();
-            switch (action) {
-                case MotionEvent.ACTION_DOWN:
-                    mStartPoint.set(ev.getX(), ev.getY());
-                    mCurrentTop = mInitTop = mHeader.getTop();
-                    mCurrentBottom = mInitBottom = mHeader.getBottom();
-                    return super.onTouchEvent(ev);
-                case MotionEvent.ACTION_MOVE:
-                    float deltaY = Math.abs(ev.getY() - mStartPoint.y);
-                    if (deltaY > 10 && deltaY > Math.abs(ev.getX() - mStartPoint.x)) {
-                        mHeader.clearAnimation();
-                        mContentView.clearAnimation();
-                        doActionMove(ev);
+                    if (mContentTop <= mHeaderCurBottom) {
+                        mHeaderView.layout(mHeadInitRect.left, mHeaderCurTop, mHeadInitRect.right, mHeaderCurBottom);
+                        mContentView.layout(mContentInitRect.left, mContentTop, mContentInitRect.right, mContentBottom);
+                        mIsMoving = true;
+                        mEnableMoving = true;
                     }
-                    break;
-                case MotionEvent.ACTION_UP:
-                    // 回滚动画
-                    if (isNeedAnimation()) {
-                        rollBackAnimation();
-                    }
-
-                    if (getScrollY() == 0) {
-                        mState = State.NORMAL;
-                    }
-
-                    isMoving = false;
-                    break;
-                default:
-                    break;
-
+                }
             }
-        }
+            break;
+            case MotionEvent.ACTION_UP: {
+                //反弹
+                if (mIsMoving) {
+                    mHeaderView.layout(mHeadInitRect.left, mHeadInitRect.top, mHeadInitRect.right, mHeadInitRect.bottom);
+                    TranslateAnimation headAnim = new TranslateAnimation(0, 0, mHeaderCurTop - mHeadInitRect.top, 0);
+                    headAnim.setDuration(200);
+                    mHeaderView.startAnimation(headAnim);
 
+                    mContentView.layout(mContentInitRect.left, mContentInitRect.top, mContentInitRect.right, mContentInitRect.bottom);
+                    TranslateAnimation contentAnim = new TranslateAnimation(0, 0, mContentTop - mContentInitRect.top, 0);
+                    contentAnim.setDuration(200);
+                    mContentView.startAnimation(contentAnim);
+                    mIsMoving = false;
+                }
+                mEnableMoving = false;
+                mIsLayout = false;
+            }
+            break;
+        }
         // 禁止控件本身的滑动.
-        boolean isHandle = isMoving;
-        if (!isMoving) {
-            try {
-                isHandle = super.onTouchEvent(ev);
-            } catch (Exception e) {
-                Log.w(LOG_TAG, e);
-            }
-        }
-        return isHandle;
+        //这句厉害,如果mEnableMoving返回TRUE,那么就不会执行super.onTouchEvent(event)
+        //只有返回FALSE的时候,才会执行super.onTouchEvent(event)
+        //禁止控件本身的滑动，就会让它，本来应有的滑动就不会滑动了，比如向上滚动
+        return mEnableMoving || super.onTouchEvent(event);
     }
 
-    /**
-     * 执行移动动画
-     *
-     * @param event
-     */
-    private void doActionMove(MotionEvent event) {
-        // 当滚动到顶部时，将状态设置为正常，避免先向上拖动再向下拖动到顶端后首次触摸不响应的问题
-        if (getScrollY() == 0) {
-            mState = State.NORMAL;
-
-            // 滑动经过顶部初始位置时，修正Touch down的坐标为当前Touch点的坐标
-            if (isTop) {
-                isTop = false;
-                mStartPoint.y = event.getY();
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            //保存原始位置
+            mTouchPoint.set((int) event.getX(), (int) event.getY());
+            mHeadInitRect.set(mHeaderView.getLeft(), mHeaderView.getTop(), mHeaderView.getRight(), mHeaderView.getBottom());
+            mContentInitRect.set(mContentView.getLeft(), mContentView.getTop(), mContentView.getRight(), mContentView.getBottom());
+            mIsMoving = false;
+            //如果当前不是从初始化位置开始滚动的话，就不让用户拖拽
+            if (getScrollY() == 0){
+                mIsLayout = true;
             }
-        }
-
-        float deltaY = event.getY() - mStartPoint.y;
-
-        // 对于首次Touch操作要判断方位：UP OR DOWN
-        if (deltaY < 0 && mState == State.NORMAL) {
-            mState = State.UP;
-        } else if (deltaY > 0 && mState == State.NORMAL) {
-            mState = State.DOWN;
-        }
-
-        if (mState == State.UP) {
-            deltaY = deltaY < 0 ? deltaY : 0;
-
-            isMoving = false;
-
-        } else if (mState == State.DOWN) {
-            if (getScrollY() <= deltaY) {
-                isMoving = true;
-            }
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            //如果当前的事件是我们要处理的事件时，比如现在的下拉，这时候，我们就不能让子控件来处理这个事件
+            //这里就需要把它截获，不传给子控件，更不能让子控件消费这个事件
+            //不然子控件的行为就可能与我们的相冲突
+            int deltaY = (int) event.getY() - mTouchPoint.y;
+//          deltaY = deltaY < 0 ? 0 : (deltaY > mHeaderView.getHeight() ? mHeaderView.getHeight() : deltaY);
             deltaY = deltaY < 0 ? 0 : (deltaY > mHeaderHeight ? mHeaderHeight : deltaY);
-        }
-
-        if (isMoving) {
-            // 初始化content view矩形
-            if (mContentRect.isEmpty()) {
-                // 保存正常的布局位置
-                mContentRect.set(mContentView.getLeft(), mContentView.getTop(), mContentView.getRight(),
-                        mContentView.getBottom());
-            }
-
-            // 计算header移动距离(手势移动的距离*阻尼系数*0.5)
-            float headerMoveHeight = deltaY * 0.5f * SCROLL_RATIO;
-            mCurrentTop = (int) (mInitTop + headerMoveHeight);
-            mCurrentBottom = (int) (mInitBottom + headerMoveHeight);
-
-            // 计算content移动距离(手势移动的距离*阻尼系数)
-            float contentMoveHeight = deltaY * SCROLL_RATIO;
-
-            // 修正content移动的距离，避免超过header的底边缘
-            int headerBottom = mCurrentBottom - mHeaderVisibleHeight;
-            int top = (int) (mContentRect.top + contentMoveHeight);
-            int bottom = (int) (mContentRect.bottom + contentMoveHeight);
-
-            if (top <= headerBottom) {
-                // 移动content view
-                mContentView.layout(mContentRect.left, top, mContentRect.right, bottom);
-
-                // 移动header view
-                mHeader.layout(mHeader.getLeft(), mCurrentTop, mHeader.getRight(), mCurrentBottom);
+            if (deltaY > 0 && deltaY >= getScrollY()&& getScrollY() == 0) {
+                onTouchEvent(event);
+                return true;
             }
         }
-    }
-
-    private void rollBackAnimation() {
-        TranslateAnimation tranAnim = new TranslateAnimation(0, 0,
-                Math.abs(mInitTop - mCurrentTop), 0);
-        tranAnim.setDuration(200);
-        mHeader.startAnimation(tranAnim);
-
-        mHeader.layout(mHeader.getLeft(), mInitTop, mHeader.getRight(), mInitBottom);
-
-        // 开启移动动画
-        TranslateAnimation innerAnim = new TranslateAnimation(0, 0, mContentView.getTop(), mContentRect.top);
-        innerAnim.setDuration(200);
-        mContentView.startAnimation(innerAnim);
-        mContentView.layout(mContentRect.left, mContentRect.top, mContentRect.right, mContentRect.bottom);
-
-        mContentRect.setEmpty();
-
-        // 回调监听器
-        if (mCurrentTop > mInitTop + TURN_DISTANCE && mOnTurnListener != null) {
-            mOnTurnListener.onTurn();
-        }
-    }
-
-    /**
-     * 是否需要开启动画
-     */
-    private boolean isNeedAnimation() {
-        return !mContentRect.isEmpty() && isMoving;
-    }
-
-    /**
-     * 翻转事件监听器
-     *
-     * @author markmjw
-     */
-    public interface OnTurnListener {
-        /**
-         * 翻转回调方法
-         */
-        public void onTurn();
+        return super.onInterceptTouchEvent(event);
     }
 }
