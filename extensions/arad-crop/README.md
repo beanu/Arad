4fbc72be18622b638ca46fbe0106d4bd83263525
beanu <zhaoyunhello@gmail.com>
2016-05-05 18:00:02 +0800

    begin V0.9.0

--- extensions/arad-crop/README.md
+++
@@ -1,7 +0,0 @@
-arad-crop
-====
-
-arad-crop功能
-=========
-*提供图片的剪切功能
-*没有使用github上的android-crop，因为有OOM的bug
--- extensions/arad-indexable-listview/src/com/beanu/arad/indexableListView/widget/ContactItemInterface.java
+++
@@ -1,9 +0,0 @@
-package com.beanu.arad.indexableListView.widget;
-
-public interface ContactItemInterface {
-    // 根据该字段来排序
-    public String getItemForIndex();
-
-    // 该字段用来显示出来
-    public String getDisplayInfo();
-}
