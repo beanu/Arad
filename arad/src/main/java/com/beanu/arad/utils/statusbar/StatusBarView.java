41a19c41c9a1bf75d217db583a49e613558abce9
beanu <zhaoyunhello@gmail.com>
2016-11-19 17:22:00 +0800

    添加statusbar 辅助类 https://github.com/laobie/StatusBarUtil

---
+++ arad/src/main/java/com/beanu/arad/utils/statusbar/StatusBarView.java
@@ -0,0 +1,19 @@
+package com.beanu.arad.utils.statusbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Beanu on 16/11/19.
+ */
+
+public class StatusBarView extends View {
+    public StatusBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StatusBarView(Context context) {
+        super(context);
+    }
+}
