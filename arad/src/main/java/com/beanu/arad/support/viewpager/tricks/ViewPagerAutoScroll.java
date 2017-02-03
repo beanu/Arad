1cec572aaaa239e538b89062eb28bfc98d8ca1e6
beanu <zhaoyunhello@gmail.com>
2017-02-03 14:15:22 +0800

    viewpager自动播放可以重复start

--- arad/src/main/java/com/beanu/arad/support/viewpager/tricks/ViewPagerAutoScroll.java
+++ arad/src/main/java/com/beanu/arad/support/viewpager/tricks/ViewPagerAutoScroll.java
@@ -6,6 +6,7 @@ import android.support.v4.view.ViewPager;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,12 +15,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class ViewPagerAutoScroll {
 
-    ViewPager mViewPager;
+    private ViewPager mViewPager;
 
     //banner自动滑动
     private int currentItem = 0;
     private boolean play = false;
     private ScheduledExecutorService scheduledExecutorService;
+    private ScheduledFuture mFuture;
+
+    //翻页延时时间 和 周期事件
+    private long initialDelay = 3, period = 5;
 
     //handle
     private Handler handler = new Handler(new Handler.Callback() {
@@ -52,6 +57,15 @@ public class ViewPagerAutoScroll {
     }
 
     public ViewPagerAutoScroll() {
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+    }
+
+    public ViewPagerAutoScroll(long initialDelay, long period) {
+        this.initialDelay = initialDelay;
+        this.period = period;
+
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
     }
 
     public void setmViewPager(ViewPager mViewPager) {
@@ -61,17 +75,32 @@ public class ViewPagerAutoScroll {
 
     public void start() {
         if (!play) {
-            scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-            // 当Activity显示出来后，每两秒钟切换一次图片显示
-            scheduledExecutorService.scheduleAtFixedRate(new ScrollTask(), 3, 5, TimeUnit.SECONDS);
+            // 当Activity显示出来后，每五秒钟切换一次图片显示
+            mFuture = scheduledExecutorService.scheduleAtFixedRate(new ScrollTask(), initialDelay, period, TimeUnit.SECONDS);
             play = true;
         }
     }
 
+    /**
+     * 停止播放，可以重复start
+     */
     public void stop() {
         if (play) {
-            // 当Activity不可见的时候停止切换
-            scheduledExecutorService.shutdown();
+            if (mFuture != null && !mFuture.isCancelled()) {
+                mFuture.cancel(true);
+            }
+            play = false;
+        }
+    }
+
+    /**
+     * 停止播放，不可以再此开启
+     */
+    public void destory() {
+        if (play) {
+            if (!scheduledExecutorService.isShutdown()) {
+                scheduledExecutorService.shutdown();
+            }
             play = false;
         }
     }
